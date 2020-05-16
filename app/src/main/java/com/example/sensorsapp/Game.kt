700a340cd6187a98d6d4c118ae4d58d6f3f387c9
ilaydagurbak <ilaydagurbak@gmail.com>
2020-05-16 19:36:02 +0200

    Gps

--- app/src/main/java/com/example/sensorsapp/Game.kt
+++ app/src/main/java/com/example/sensorsapp/Game.kt
@@ -1,8 +1,10 @@
 package com.example.sensorsapp
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.OvalShape
 import android.hardware.Sensor
@@ -10,23 +12,34 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
+import android.os.Handler
+import android.util.DisplayMetrics
 import android.view.Menu
-
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatImageView
 
 class Game : Activity(), SensorEventListener {
     private lateinit var sensorManager: SensorManager
     private lateinit var accelerometer: Sensor
-    private var lastUpdate: Long = 0
     private lateinit var animatedView: AnimatedView
-    var ballShape = ShapeDrawable(OvalShape())
-
+    private var ballShape = ShapeDrawable(OvalShape())
     private var touchScreen = false
+    private var displayWidth = 0
+    private var displayHeight = 0
+    private var lastUpdate: Long = 0
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        counter = 0
+        animatedView = AnimatedView(this)
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        displayWidth = displayMetrics.widthPixels - 100
+        displayHeight = displayMetrics.heightPixels - 100
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        accelerometer = sensorManager
-            .getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         lastUpdate = System.currentTimeMillis()
         animatedView = AnimatedView(this)
         setContentView(animatedView)
@@ -55,30 +68,70 @@ class Game : Activity(), SensorEventListener {
 
     override fun onSensorChanged(event: SensorEvent) {
         if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
-            x -= event.values[0].toInt()
-            y += event.values[1].toInt()
+            xAcceleration -= event.values[0].toInt()
+            yAcceleration += event.values[1].toInt()
+            if (xAcceleration > displayWidth) {
+                xAcceleration = displayWidth
+                gameOver()
+
+            } else if (xAcceleration < 0) {
+                xAcceleration = 0
+                gameOver()
+            }
+            if (yAcceleration > displayHeight - 100) {
+                yAcceleration = displayHeight - 100
+                gameOver()
+            } else if (yAcceleration < 0) {
+                yAcceleration = 0
+                gameOver()
+            }
         }
     }
 
-    inner class AnimatedView(context: Context?) :
-        androidx.appcompat.widget.AppCompatImageView(context) {
+    private fun  gameOver() {
+        touchScreen = true
+        counter++
+        resetGame()
+        if (counter > 0) {
+            onPause()
+            Toast.makeText(applicationContext, "Game over!!", Toast.LENGTH_SHORT).show()
+            val handler = Handler()
+            handler.postDelayed({
+                val intent = Intent(this@Game, MainActivity::class.java)
+                startActivity(intent)
+            }, 1000)
+        }
+    }
+
+    private fun resetGame() {
+        yAcceleration = displayHeight / 2
+        xAcceleration = displayWidth / 2
+    }
+
+    inner class AnimatedView(context: Context?) : AppCompatImageView(context) {
+        var p = Paint()
         override fun onDraw(canvas: Canvas) {
-            ballShape.setBounds(
-                Game.x,
-                Game.y,
-                Game.x + Companion.width,
-                Game.y + Companion.height
-            )
+            p.color = Color.BLACK
+            p.textSize = 80f
+            ballShape.setBounds(xAcceleration, yAcceleration, xAcceleration + Companion.width, yAcceleration + Companion.height)
+            canvas.drawColor(Color.parseColor("#d1d8e0"))
             ballShape.draw(canvas)
-            ballShape.paint.color = Color.parseColor("#D53B96")
             invalidate()
+        }
+
+        init {
+            ballShape = ShapeDrawable(OvalShape())
+            ballShape.paint.color = Color.parseColor("#D53B96")
         }
     }
 
     companion object {
-        const val width: Int = 120
-        const val height: Int = 120
+        const val width: Int = 100
+        const val height: Int = 100
         var x = 0
         var y = 0
+        var xAcceleration = 0
+        var yAcceleration = 0
+        var counter = 0
     }
 }
